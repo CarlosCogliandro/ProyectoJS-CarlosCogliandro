b292e655d8c40449fb25f276e352af0b33cddad5
Carlos Cogliandro <carloscogliandro22@gmail.com>
2022-08-09 16:27:19 +0200

    Acomodando el codigo y creando html desde js

---
+++ js/maindonaciones.js
@@ -0,0 +1,41 @@
+let nav = document.getElementById('navDona');
+
+
+// NAVBAR
+
+function mostrarNavDona() {
+    const dona1 = document.createElement('dona1');
+    dona1.classList.add('navbar');
+    dona1.innerHTML =  `<div>
+                        <ul class="navbar1">
+                            <li><a class="link" href="../index.html">HOME</a></li>
+                            <li><a class="link" href="">REWARDS</a></li>
+                            <li><a class="link" href="">RULES</a></li>
+                            <li><a class="link" href="./donaciones.html">DONATION</a></li>
+                            <li><a class="link" href="">SUPPORT</a></li>
+                        </ul>
+                        </div>
+
+                        <div>
+                        <ul class="redessociales">
+                            <li><a class="redes1" href="https://www.facebook.com/"><img class="redes" src="../img/redes/facebook.png" alt="facebook"></a></li>
+                            <li><a class="redes1" href="https://www.twitter.com/"><img class="redes" src="../img/redes/gorjeo.png" alt="twitter"></a></li>
+                            <li><a class="redes1" href="https://www.instagram.com/"><img class="redes" src="../img/redes/instagram.png" alt="instagram"></a></li>
+                            <li><a class="redes1" href="https://www.twitch.com/"><img class="redes" src="../img/redes/twitch.png" alt="twitch"></a></li>
+                            <li><a class="redes1" href="https://www.youtube.com/"><img class="redes" src="../img/redes/youtube.png" alt="youtube"></a></li>
+                        </ul>
+                        </div>
+
+                        <button id="boton-carrito"> <i class="fas fa-shopping-cart"></i><span id="contadorCarrito">0</span></button>
+                        `
+
+    nav.appendChild(dona1);
+}
+
+mostrarNavDona();
+
+
+
+
+
+
